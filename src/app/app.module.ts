187d5f96b988c33694b0d1541976779d1a342688
Zoechbauer <hans.zoechbauer@aon.at>
2020-05-08 12:22:57 +0200

    Angular Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products.component';
 import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, ProductComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductComponent,
+    HomeComponent,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
